01be880d86c8304e3b0bc27effc41f119fd9c353
DevEduBiel <eduardo.vitorino@sptech.school>
2023-03-15 11:43:24 -0300

    BoardException e programação defensiva

---
+++ src/src/boardgame/BoarException.java
@@ -0,0 +1,9 @@
+package src.boardgame;
+
+public class BoarException extends RuntimeException {
+    private static final long serialVersion =1L;
+
+    public BoarException(String message) {
+        super(message);
+    }
+}
--- src/src/boardgame/Board.java
+++ src/src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece [][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows<1 && columns<1){
+            throw new BoarException("Error creating board:rows and colums");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -15,28 +18,45 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
 
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
     public Piece piece(int rows,int columns){
+        if (!positionExists(rows,columns)){
+            throw new BoarException("Error creating board:position does not exist");
+        }
         return pieces[rows][columns];
     }
 
     public Piece piece (Position position){
+        if (!positionExists(position)){
+            throw new BoarException("Error creating board:position does not exist");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public  void  placePiece (Piece piece ,Position position){
+        if(thereIsAPiece(position)){
+            throw new BoarException("Error creating board:position already exists");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row <rows && column >= 0 && column <columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(),position.getColumn());
+
+    }
+    public boolean thereIsAPiece(Position position){
+        if (!positionExists(position)){
+            throw new BoarException("Error creating board:position does not exist");
+        }
+        return piece(position) != null;
+    }
 }
