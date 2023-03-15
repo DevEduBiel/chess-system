974d6fb176fe26a3b3e5101af826f982221dee2b
DevEduBiel <eduardo.vitorino@sptech.school>
2023-03-15 12:05:59 -0300

    ChessException e ChessPosition

---
+++ src/src/chess/ChessException.java
@@ -0,0 +1,10 @@
+package src.chess;
+
+public class ChessException extends RuntimeException{
+    private static final long serialVersionUID =1L;
+
+    public ChessException(String message){
+        super(message);
+    }
+    
+}
