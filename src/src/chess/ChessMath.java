76955e3ed693b458b4613ca6b9dc8792323c656d
DevEduBiel <eduardo.vitorino@sptech.school>
2023-03-17 15:14:33 -0300

    Contagem de movimento das peças

--- src/src/chess/ChessMath.java
+++ src/src/chess/ChessMath.java
@@ -82,7 +82,8 @@ public class ChessMath {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturePiece = board.removePiece(target);
         board.placePiece(p, target);
         if (capturePiece != null) {
@@ -93,7 +94,8 @@ public class ChessMath {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece)board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
         if (capturedPiece != null) {
             board.placePiece(capturedPiece, source);
--- src/src/chess/ChessPiece.java
+++ src/src/chess/ChessPiece.java
@@ -9,6 +9,8 @@ public abstract class ChessPiece extends Piece {
 
     private Color color;
 
+    private int moveCount;
+
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
@@ -16,6 +18,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor() {
         return color;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
     }
 
     public ChessPosition getChessPosition() {
